9fd394278a3392777abe8911c9457b59975568db
SeanMugo <sean.mugo08@Gmail.com>
2026-07-03 23:14:13 +0300

    chore: configure application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
-    <h1>Welcome to SlotSmart 🚗</h1>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
